15bc4df46161caf3f3ffa3f3b30f83dc55bdf20c
haldokoko <haldokoko@gmail.com>
2022-06-11 15:37:11 +0800

    bismillah

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (respon) =>{
+  try {
+    const [resIXX, resvgc] = await Promise.all([promiseTheaterIXX(), promiseTheaterVGC()]);
+    const result = await [...resIXX,...resvgc] ;
+    return result.filter(item => item.hasil === respon).length
+}catch (error) {
+    console.log("ada eror bosku: " + error);
+}
+};
 
 module.exports = {
   promiseOutput,
